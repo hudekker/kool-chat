ebc5533e3e8237a7293a94c673403468d5a5f87d
robert hudek <hudektech@gmail.com>
2022-09-18 00:47:11 +0800

    4

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// var socket = io();
-
 let formMsg = document.getElementById('form-msg');
 let inputMsg = document.getElementById('input-msg');
 let formPartnerId = document.getElementById('form-partner-id');
@@ -30,12 +28,6 @@ const connOpen = (conn, boolReceiver = false) => {
     conn.on("open", () => {
       displayMsg(`Friend Id = ${conn.peer}`, 'admin-msg');
       displayMsg(`Connected with ${conn.peer}`, 'admin-msg');
-      // if (boolReceiver) {
-      //   // displayMsg(`Received connection request from ${conn.peer}`, "", false);
-      //   displayMsg(`Friend Id = ${conn.peer}`, 'admin-msg');
-      // } else {
-      //   displayMsg(`Connected with ${conn.peer}`, 'admin-msg');
-      // }
       resolve();
     });
   });
@@ -48,12 +40,9 @@ const receiveConnRequest = async (conn2) => {
 
   let boolReceiver = true;
   await connOpen(conn, boolReceiver);
-  console.log('finished waiting for receive conn request');
 
   // Keep this event listener open, will receive data multiple times
-  conn.on("data", (data) => {
-    displayMsg(`${data} `, 'friend-msg');
-  });
+  conn.on("data", (data) => displayMsg(`${data} `, 'friend-msg'));
 
   conn.on("close", () => {
     displayMsg(`${conn.peer} left the chat`, 'admin-msg');
@@ -74,8 +63,6 @@ const receiveConnRequest = async (conn2) => {
 
 const sendConnRequest = async () => {
 
-  // let partnerId = '';
-
   // Create the conn
   conn = peer.connect(partnerId);
   // displayMsg(`Sending connection request to ${partnerId} `, 'admin-msg');
@@ -83,11 +70,8 @@ const sendConnRequest = async () => {
   // Wait for the conn to open and add it to the dropdown
   let boolReceiver = false;
   await connOpen(conn, boolReceiver);
-  console.log('Sent Conn request and finished waiting for connOpen');
   // Keep this event listener open, will receive data multiple times
-  conn.on("data", (data) => {
-    displayMsg(`${data} `, 'friend-msg');
-  });
+  conn.on("data", (data) => displayMsg(`${data} `, 'friend-msg'));
 
   conn.on("close", () => {
     displayMsg(`${partnerId} left the chat`, 'admin-msg');
@@ -100,9 +84,6 @@ const sendConnRequest = async () => {
     conn.close();
     conn = "";
   });
-
-  // Event listener tied to this conn
-  // sendMsg.addEventListener("click", () => sendMessage(conn));
 };
 
 
@@ -119,8 +100,6 @@ window.onload = async (event) => {
     secure: true,
   });
 
-  debugger;
-
   peerId = await getPeerId(peer);
 
   await waitForPeer();
@@ -131,8 +110,6 @@ window.onload = async (event) => {
   // Receive conn request
   peer.on("connection", receiveConnRequest);
 
-  // // Or send conn request
-  // sendConnRequest();
 }
 
 formMsg.addEventListener('submit', function (e) {
@@ -148,23 +125,13 @@ formPartnerId.addEventListener('submit', function (e) {
   e.preventDefault();
 });
 
-// socket.on('chat message', function (msg) {
-//   var item = document.createElement('li');
-//   var div = document.createElement('div');
-//   div.classList.add('shift')
-//   item.appendChild(div)
-//   // item.textContent = msg;
-//   div.textContent = msg;
-//   messages.appendChild(item);
-//   window.scrollTo(0, document.body.scrollHeight);
-// });
 
 const displayMsg = (msg, msgClass) => {
   let item = document.createElement('li');
   let div = document.createElement('div');
   item.classList.add(msgClass)
   item.appendChild(div)
-  // item.textContent = msg;
+
   div.textContent = msg;
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
@@ -190,11 +157,7 @@ window.onclick = function (event) {
 
 btnPartnerId.onclick = function () {
   partnerId = document.querySelector('#input-partner-id').value;
-  if (partnerId !== '') {
-    console.log(partnerId);
-    // displayMsg(`Friend ID = ${partnerId} `, `admin-msg`)
-    sendConnRequest();
-  }
+  if (partnerId !== '') sendConnRequest();
   modal.classList.add('modal-hide');
 }
 
